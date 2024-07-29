2a24a674034a017d4825bf961fd161b8f86ec553
Santi Sevitz <santisevitz@hotmail.com>
2024-07-29 09:10:34 -0300

    navbar impl

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Async Landing Page
 
 This is a landing page dedicated to my service to provide people with 3D printing services
+
+## Installs:
+**Tailwind:** <br>
+```
+npm install -D tailwindcss <br>
+npx tailwindcss init
+```
+
+**React Icons:** <br>
+```
+npm install react-icons --save
+```
+
+**Mobile Swipe Functionality** <br>
+```
+npm install @use-gesture/react react-spring
+```
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,11 @@
+import React, { useState } from 'react';
 import asyncLogo from '../assets/async_logo_5.png';
+import asyncLogoMobile from '../assets/async_logo_4_white.png';
+import navBar_bg from '../assets/navbar_bg.png';
 import languages from '../languages';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useSpring, animated } from 'react-spring';
+import { useDrag } from '@use-gesture/react';
 
 interface NavBarProps {
     language: string;
@@ -8,24 +14,78 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({ language }) => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
     const texts = languages[language] || languages.es;
 
     const navBarTexts = [texts.services, texts.questions, texts.about_us];
 
+    const [{ x }, api] = useSpring(() => ({ x: -300 }));
+
+    const toggleMenu = () => {
+        if (isOpen) {
+            api.start({ x: -300 });
+            setIsOpen(false);
+        } else {
+            api.start({ x: 0 });
+            setIsOpen(true);
+        }
+    };
+
+    const bind = useDrag(({ down, movement: [mx] }) => {
+        if (down && mx < 0) {
+            api.start({ x: mx });
+        } else if (!down && mx < -50) {
+            api.start({ x: -300 });
+            setIsOpen(false);
+        } else if (!down) {
+            api.start({ x: 0 });
+            setIsOpen(true);
+        }
+    });
+
+
+
     return (
         <>
-            <div className="w-full hover:bg-blue-600 hover:opacity-80 fixed top-0 left-0 z-50 h-16 py-12 transition-all duration-500 ease-in-out ">
-                <div className="flex justify-between items-center h-full  pl-20 pr-16 text-white text-lg  font-medium font-montserrat">
-                    <img src={asyncLogo} alt="Async Logo" className='h-14 hover:cursor-pointer' />
-                    <div className='flex justify-normal gap-x-12'>
-                        {navBarTexts.map((text, index) => (
-                            <div key={index} className='hover:text-gray-300 hover:cursor-pointer'>{text}</div>
-                        ))}
-                        <div className='font-semibold ml-5 hover:cursor-pointer'>[{texts.current_language}]</div>
+            <div className="w-full bg-gradient-to-b from-blue-600 from-10% via-10% to-blue-900/0 fixed top-0 left-0 z-50 h-16 py-12">
+                <div className="relative w-full h-full">
+                    <div className="flex justify-between items-center h-full pl-5 pr-5 md:pl-20 md:pr-16 text-white text-lg font-medium font-montserrat relative">
+                        <div className="md:hidden flex items-center">
+                            <button onClick={toggleMenu} className="focus:outline-none">
+                                <GiHamburgerMenu className="w-11 h-11 text-white" />
+                            </button>
+                        </div>
+                        <div className="h-14 mx-auto md:mx-0 md:ml-0 hover:cursor-pointer">
+                            <img src={asyncLogoMobile} alt="Async Logo Mobile" className="block md:hidden h-16 rotate-12 -mt-1" />
+                            <img src={asyncLogo} alt="Async Logo" className="hidden md:block h-full" />
+                        </div>
+                        <div className="hidden md:flex md:justify-normal md:gap-x-12">
+                            {navBarTexts.map((text, index) => (
+                                <div key={index} className="hover:text-xl hover:cursor-pointer transition-all duration-500 ease-in-out">
+                                    {text}
+                                </div>
+                            ))}
+                            <div className="font-semibold ml-5 hover:cursor-pointer">{texts.current_language}</div>
+                        </div>
+                        <div className="md:hidden font-semibold text-3xl hover:cursor-pointer">{texts.current_language}</div>
                     </div>
-
                 </div>
             </div>
+
+            <animated.div
+                {...bind()}
+                style={{ x, touchAction: 'none' }}
+                className="md:hidden fixed top-0 left-0 w-3/5 h-fit pb-10 rounded-r-3xl bg-blue-600 bg-opacity-55 z-40 transform transition-transform duration-75"
+            >
+                <div className="flex flex-col mt-20 gap-3 items-start pl-5 pr-5 text-white text-xl font-medium font-montserrat">
+                    {navBarTexts.map((text, index) => (
+                        <div key={index} className="py-2 hover:text-gray-300 hover:cursor-pointer">
+                            {text}
+                        </div>
+                    ))}
+                </div>
+            </animated.div>
         </>
     )
 
