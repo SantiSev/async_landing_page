0524bd33900624d63dbc10295ae226d4c2b4f80e
sev <santisevitz@hotmail.com>
2024-12-30 12:33:10 -0300

    changes to the install sh and bat files

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,14 +2,20 @@
 echo "Installing dependencies..."
 # Install Tailwind CSS as a dev dependency
 npm install -D tailwindcss
-echo "install [ 1 / 4 ] done"
+echo "install [ 1 / 6 ] done"
 # Initialize Tailwind CSS
 npx tailwindcss init
-echo "install [ 2 / 4 ] done"
+echo "install [ 2 / 6 ] done"
 # Install React Icons
 npm install react-icons --save
-echo "install [ 3 / 4 ] done"
+echo "install [ 3 / 6 ] done"
 # Install use-gesture and react-spring
 npm install @use-gesture/react react-spring
-echo "install [ 4 / 4 ] done"
+echo "install [ 4 / 6 ] done"
+# Install Slick Image Slider
+npm install react-slick --save
+echo "install [ 5 / 6 ] done"
+# Install AOS
+npm install aos --save
+echo "install [ 6 / 6 ] done"
 echo "All dependencies have been installed successfully."
